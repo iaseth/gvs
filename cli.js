52194cda40f4c8fcb3f1b39c970c080e77a19431
Ankur Seth <iankurseth@gmail.com>
2023-08-24 17:13:43 +0530

    Now gvs can compile njk file to svg, published gvs@0.3.0

--- cli.js
+++ cli.js
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
+const fs = require("fs");
+const nunjucks = require("nunjucks");
 
-console.log("Welcome to GVS!");
+
+
+function doStuff (filePath) {
+	const svgOutputFilePath = filePath.slice(0, -4) + ".svg";
+	const njkContents = fs.readFileSync(filePath, "utf8");
+
+	const svgContents = nunjucks.renderString(njkContents);
+	fs.writeFileSync(svgOutputFilePath, svgContents);
+	console.log(`Saved: '${svgOutputFilePath}'`);
+}
+
+function main (args) {
+	if (args.length === 0) {
+		console.log("No args supplied!");
+	} else if (args.length === 1) {
+		const filePath = args[0];
+		if (fs.existsSync(filePath)) {
+			console.log(`Path found: '${filePath}'`);
+			if (filePath.endsWith(".njk")) {
+				doStuff(filePath);
+			}
+		} else {
+			console.log(`Path NOT found: '${filePath}'`);
+		}
+	} else {
+		console.log("Too many args supplied!");
+	}
+}
+
+const args = process.argv.slice(2);
+main(args);
