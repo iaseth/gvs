9f221598e88f8913ae6dde667ad5efa743b30616
Ankur Seth <iankurseth@gmail.com>
2023-08-24 17:26:46 +0530

    Now trimming trailing whitespace and removing empty lines from SVG, published gvs@0.4.0

--- cli.js
+++ cli.js
@@ -8,7 +8,12 @@ function doStuff (filePath) {
 	const svgOutputFilePath = filePath.slice(0, -4) + ".svg";
 	const njkContents = fs.readFileSync(filePath, "utf8");
 
-	const svgContents = nunjucks.renderString(njkContents);
+	const svgRawText = nunjucks.renderString(njkContents);
+	const svgRawLines = svgRawText.split("\n");
+	const svgTrimmedLines = svgRawLines.map(line => line.trimEnd()); // trim trailing whitespace
+	const svgNotEmptyLines = svgTrimmedLines.filter(line => line); // remove empty lines
+	const svgContents = svgNotEmptyLines.join("\n");
+
 	fs.writeFileSync(svgOutputFilePath, svgContents);
 	console.log(`Saved: '${svgOutputFilePath}'`);
 }
